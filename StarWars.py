6357b24b97fa92ca45f45688576bf53634305516
ilia <schabaevil@yandex.ru>
2021-02-13 12:44:21 +0300

    4

--- StarWars.py
+++ StarWars.py
@@ -31,3 +31,71 @@ PHOTOS = {name: image.load(KARTINKI + '{}.png'.format(name)).convert_alpha()
 POSSITION_OGRAD = 450
 POSSITION_VRAGOV = 65
 SDVIG_VRAGOV = 35
+
+class Hero(sprite.Sprite):
+    def __init__(self):
+        sprite.Sprite.__init__(self)
+        self.image = PHOTOS['hero']
+        self.rect = self.image.get_rect(topleft=(375, 540))
+        self.speed = 5
+
+    def update(self, keys, *args):#  ДВИЖЕНИЕ КОРАБЛЯ ИГРОКА В ЗАДАННОЙ ОБЛАСТИ
+        if keys[K_LEFT] and self.rect.x > 10:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < 740:
+            self.rect.x += self.speed
+        game.screen.blit(self.image, self.rect)
+
+
+class Rocket(sprite.Sprite):
+    def __init__(self, xpos, ypos, direction, speed, filename, side):
+        sprite.Sprite.__init__(self)
+        self.image = PHOTOS[filename]
+        self.rect = self.image.get_rect(topleft=(xpos, ypos))
+        self.speed = speed
+        self.direction = direction
+        self.side = side
+        self.filename = filename
+
+    def update(self, keys, *args):
+        game.screen.blit(self.image, self.rect)
+        self.rect.y += self.speed * self.direction
+        if self.rect.y < 15 or self.rect.y > 600:
+            self.kill()
+
+
+class Vragi(sprite.Sprite):
+    def __init__(self, row, column):
+        sprite.Sprite.__init__(self)
+        self.row = row
+        self.column = column
+        self.images = []
+        self.load_images()
+        self.index = 0
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect()
+
+
+    def update(self, *args):
+        game.screen.blit(self.image, self.rect) #ОБНОВЛНИЕ ПОЗИЦИЙ ПРОТИВНИКОВ
+
+    def load_images(self):
+        images = {0: '1',
+                  1: '2',
+                  2: '2',
+                  3: '3',
+                  4: '3'
+                  }
+        img1 = (PHOTOS['enemy{}'.format(images[self.row])])
+        self.images.append(transform.scale(img1, (40, 35)))
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    game = StarWars()
+    game.main()
